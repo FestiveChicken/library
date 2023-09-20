967e66f534915b993b3a11928db0b328a1104c68
Uesli Keta <44416957+FestiveChicken@users.noreply.github.com>
2023-09-20 11:54:37 -0400

    Finished backend
    Added read button to show wether or not book has been read. Remove button fully works.

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const addNewBook = document.getElementById('addNewBook')
 const outputBox = document.querySelector("output")
 const formInfo = document.getElementById('formInfo')
 const cancelButton = document.getElementById('cancel')
-const removeButton = document.getElementById('removeButton')
+
 let i = 0
 
 
 //Adds info to book
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read = false) {
     this.title = title
     this.author = author
     this.pages = pages
@@ -18,34 +18,63 @@ function Book(title, author, pages, read) {
 
 //Adds book title to array
 function addBookToLibrary (title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read))
+    myLibrary.push(new Book(title, author, pages))
 }
 
 //Creates Card for each book
 function createCard() {
-    var cardHolder = document.getElementById('cardHolder')
-    var card = document.createElement('div')
+    let cardHolder = document.getElementById('cardHolder')
+    let card = document.createElement('div')
     card.setAttribute('class', 'card')
-    card.setAttribute('id', i)
     card.style.backgroundColor = 'grey'
     card.style.margin = '10px'
     card.style.padding = '10px'
     cardHolder.appendChild(card)
 
     //Card Content
-    var cardContent = document.createElement('p')
+    let cardContent = document.createElement('p')
     cardContent.textContent = myLibrary[i].title + "\n" + myLibrary[i].author + "\n" + myLibrary[i].pages + "\n"
     card.appendChild(cardContent)
 
     //Remove Button
-    var removeButton = document.createElement('button')
-    removeButton.setAttribute('id', 'removeButton')
+    let removeButton = document.createElement('button')
+    removeButton.setAttribute('id',[i])
     removeButton.textContent = 'Remove'
+    card.appendChild(removeButton)
+
+
+    //Read Button
+    let readButton = document.createElement('button')
+    readButton.textContent = 'Unread'
+    readButton.style.backgroundColor = 'red'
+    readButton.style.marginLeft = '10px'
+    card.appendChild(readButton)
+
     //Removes Card
     removeButton.addEventListener('click', () => {
         cardHolder.removeChild(card)
-    })
-    card.appendChild(removeButton)
+    }
+    )
+
+    //Changes read status for book
+    readButton.addEventListener('click', () => {
+        let idNumber = removeButton.getAttribute('id')
+        console.log(idNumber)
+        if (myLibrary[idNumber].read == false) {
+            myLibrary[idNumber].read = true
+            readButton.textContent = 'Read'
+            readButton.style.backgroundColor = 'green'
+            console.log(myLibrary)
+        }
+        else if (myLibrary[idNumber].read == true) {
+            myLibrary[idNumber].read = false
+            readButton.style.backgroundColor = 'red'
+            readButton.textContent = 'Unread'
+            console.log(myLibrary)
+        }
+    }
+    )
+
     i++
 }
 
@@ -53,7 +82,8 @@ function createCard() {
 showButton.addEventListener('click', () => {
     formInfo.reset()
     addNewBook.showModal();
-})
+}
+)
 
 //Closes dialog and resets form
 cancelButton.addEventListener('click', () => {
